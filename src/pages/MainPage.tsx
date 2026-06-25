8a4364bbf0974ee80d9918af26108600437c4689
Нурэль <nzhakypov521@gmail.com>
2026-06-25 17:17:38 +0600

    удалён contactList, перенесено в MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,19 @@
+import { useContactStore } from "../api/useContactStorage";
 import "../App.css";
-import ContactList from "../components/ContactList";
+import ContactItem from "../components/ContactItem";
+
 
 function MainPage() {
-    return (
-        <>
-            <ContactList/>
-        </>
-    )
+    const contacts = useContactStore((state) => state.contacts);
+  return (
+    <div className="w-full h-150 flex justify-center items-center">
+      <div className="w-150 shadow-md p-5 rounded-xl flex flex-col gap-4 max-h-100 overflow-auto">
+        {contacts.map((cont) => {
+          return <ContactItem key={cont.id} contact={cont}></ContactItem>;
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default MainPage
