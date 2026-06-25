2185a50439425d57ad477bc7097abdfc2c9ffff7
Нурэль <nzhakypov521@gmail.com>
2026-06-25 17:19:42 +0600

    fix: перенёс fetchContacts в MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
 import { useContactStore } from "../api/useContactStorage";
 import "../App.css";
 import ContactItem from "../components/ContactItem";
 
-
 function MainPage() {
-    const contacts = useContactStore((state) => state.contacts);
+  const contacts = useContactStore((state) => state.contacts);
+  const fetchContacts = useContactStore((state) => state.fetchContacts);
+  useEffect(() => {
+    fetchContacts();
+  }, [fetchContacts]);
   return (
     <div className="w-full h-150 flex justify-center items-center">
       <div className="w-150 shadow-md p-5 rounded-xl flex flex-col gap-4 max-h-100 overflow-auto">
@@ -13,7 +17,7 @@ function MainPage() {
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
